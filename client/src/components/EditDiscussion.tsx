bfaee174ac4308db0cf6762992a1456467cc3886
Ford Henley <fordp86@gmail.com>
2022-10-31 14:43:38 -0500

    Updated Backed IP In Provider
    Updated Backed IP In Provider

--- client/src/components/EditDiscussion.tsx
+++ client/src/components/EditDiscussion.tsx
@@ -5,65 +5,66 @@ import DiscussionContext from '../contexts/DiscussionContext';
 
 const EditPost: React.FC = (props) => {
 
-    let params = useParams()
-  let [ changePost, setProduct ] = useState({
-    id:  params.discussionId,
-    headline: "",
-     content: "" 
-  })
-    let { editPost } = useContext(DiscussionContext);
-    let history = useHistory()
+  //   let params = useParams()
+  // let [ changePost, setProduct ] = useState({
+  //   id:  params.discussionId,
+  //   headline: "",
+  //    content: "" 
+  // })
+  //   let { editPost } = useContext(DiscussionContext);
+  //   let history = useHistory()
     
 
-    let {id, headline, content } = changePost
+  //   let {id, headline, content } = changePost
 
-    function handleChange(event:any) {
-      setProduct((preValue) => {
-        return { ...preValue, [event.target.name]: event.target.value }})
-    }
+  //   function handleChange(event:any) {
+  //     setProduct((preValue) => {
+  //       return { ...preValue, [event.target.name]: event.target.value }})
+  //   }
 
-    function handleSubmit(event: any) {
-        event.preventDefault();
-        editPost(changePost).then(() => {
-            history.push('/discussion');
-        }).catch((error: any) => {
-            console.log(error);
-            history.push('/discusion');
-        });
-    }
+  //   function handleSubmit(event: any) {
+  //       event.preventDefault();
+  //       editPost(changePost).then(() => {
+  //           history.push('/discussion');
+  //       }).catch((error: any) => {
+  //           console.log(error);
+  //           history.push('/discusion');
+  //       });
+  //   }
 
    
 
     return (
-        <IonContent fullscreen>
-            <IonGrid>
+      //   <IonContent fullscreen>
+      //       <IonGrid>
        
-        <form onSubmit={handleSubmit}>
-        <IonItem>
-          <IonLabel position="stacked">Discussion Headline</IonLabel>
-          <IonInput
-            type="text"
-            placeholder="headline"
-            name="headline"
-            value={changePost.headline}
-            onIonChange={handleChange}
-          />
-          <IonLabel position="stacked">Content</IonLabel>
-          <IonInput
-            type="text"
-            placeholder="Start Post Here"
-            name="content"
-            value={changePost.content}
-            onIonChange={handleChange}
-          />
-        </IonItem>
-        <IonButton type="submit" expand="block">
-          Confirm Edit
-        </IonButton>
-      </form>
+      //   <form onSubmit={handleSubmit}>
+      //   <IonItem>
+      //     <IonLabel position="stacked">Discussion Headline</IonLabel>
+      //     <IonInput
+      //       type="text"
+      //       placeholder="headline"
+      //       name="headline"
+      //       value={changePost.headline}
+      //       onIonChange={handleChange}
+      //     />
+      //     <IonLabel position="stacked">Content</IonLabel>
+      //     <IonInput
+      //       type="text"
+      //       placeholder="Start Post Here"
+      //       name="content"
+      //       value={changePost.content}
+      //       onIonChange={handleChange}
+      //     />
+      //   </IonItem>
+      //   <IonButton type="submit" expand="block">
+      //     Confirm Edit
+      //   </IonButton>
+      // </form>
      
-      </IonGrid>
-      </IonContent>
+      // </IonGrid>
+      // </IonContent>
+      <div></div>
     )
 };
 
